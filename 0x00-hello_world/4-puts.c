4d64e9ecc8630d7cff7f9e6dfdb8c2cdfc864c9f
CMuya <conie.muya@gmail.com>
2023-06-15 12:00:49 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that prints wih puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
