cd0024f19a22c99f27a6fbb27d08bcf0d899b712
CMuya <conie.muya@gmail.com>
2023-06-20 08:36:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,13 +11,13 @@ void print_alphabet_x10(void)
 		i = 0;
 
 
-		while (i < 10);
+		while (i < 10)
 		{
 			ch = 'a';
-			while (ch <= 'z');
+			while (ch <= 'z')
 			{
 				_putchar(ch);
-				ch++
+				ch++;
 			}
 			_putchar('\n');
 			i++;
