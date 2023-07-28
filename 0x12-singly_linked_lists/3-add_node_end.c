55603c12d15b8a444fa80a21f61a294f01f0db76
“consolata <conie.muya@gmail.com>
2023-07-28 13:29:19 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,21 +11,31 @@
  *
  * description: singly linked list node structure
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *temp = *head;
 	unsigned int len = 0;
 
-	while (str == NULL)
+	/* Check if the input string is NULL */
+	if (str == NULL)
 		return (NULL);
+
+	/* Calculate the length of the input string */
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	new_node->len = len;
 	new_node->next = NULL;
 
